6dab634e192e07b00613c6fc64980e10a458ced8
saqib147 <saqibarshad429@gmail.com>
2025-04-07 18:52:45 +0500

    Add job listing components and layout
    - Created a reusable card component for displaying job listings.
    - Implemented a listing card component to showcase individual job listings with title, company, tags, and location.
    - Added a listing tags component to display tags associated with each job listing.
    - Developed the index view for job listings, including a search feature and handling for no listings found.
    - Created a detailed view for individual job listings, including job description, contact options, and website link.
    - Designed hero and search partials for improved UI and user experience.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,12 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all(),
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 
-Route::get('/listing/{id}', function($id){
-    return view('listing', [
-        'listing' => Listing::find($id)
-    ]);
-});
-
+Route::get('/listing/{listing}', [ListingController::class, 'show']);
+   
